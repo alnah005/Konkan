390f96251ab8dac45dc58ff3d210ab6abced5a5f
alnah005 <alnah005@umn.edu>
2020-08-19 16:55:57 -0500

    added another test that tests calculating average score

--- test/player_test.dart
+++ test/player_test.dart
@@ -1,16 +1,46 @@
 import 'package:solitaire/models/player.dart';
+import 'package:solitaire/models/playing_card.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('player won', () {
-    final player = Player(PositionOnScreen.bottom);
-    expect(player.personalInfo.wins, 0);
-    expect(player.personalInfo.losses, 0);
-    player.recordGame(PositionOnScreen.bottom);
-    expect(player.personalInfo.wins, 1);
-    expect(player.personalInfo.losses, 0);
-    player.recordGame(PositionOnScreen.right);
-    expect(player.personalInfo.wins, 1);
-    expect(player.personalInfo.losses, 1);
+  group('recording game', () {
+    test('recording losses and wins', () {
+      final player = Player(PositionOnScreen.bottom);
+      expect(player.personalInfo.wins, 0);
+      expect(player.personalInfo.losses, 0);
+      player.recordGame(PositionOnScreen.bottom);
+      expect(player.personalInfo.wins, 1);
+      expect(player.personalInfo.losses, 0);
+      player.recordGame(PositionOnScreen.right);
+      expect(player.personalInfo.wins, 1);
+      expect(player.personalInfo.losses, 1);
+    });
+    test('avg score', () {
+      final player = Player(PositionOnScreen.bottom);
+      List<PlayingCard> cards = [
+        PlayingCard(cardSuit: CardSuit.diamonds, cardType: CardType.jack),
+        PlayingCard(cardSuit: CardSuit.diamonds, cardType: CardType.jack),
+        PlayingCard(cardSuit: CardSuit.diamonds, cardType: CardType.jack),
+      ];
+      player.cards = cards;
+      player.recordGame(PositionOnScreen.bottom);
+      expect(player.personalInfo.avgScore, 30.0);
+      player.recordGame(PositionOnScreen.bottom);
+      expect(player.personalInfo.avgScore, 30.0);
+      player.cards.add(
+          PlayingCard(cardSuit: CardSuit.diamonds, cardType: CardType.five));
+      player.cards.add(
+          PlayingCard(cardSuit: CardSuit.diamonds, cardType: CardType.five));
+      player.cards.add(
+          PlayingCard(cardSuit: CardSuit.diamonds, cardType: CardType.five));
+      player.cards.add(
+          PlayingCard(cardSuit: CardSuit.diamonds, cardType: CardType.five));
+      player.cards.add(
+          PlayingCard(cardSuit: CardSuit.diamonds, cardType: CardType.five));
+      player.cards.add(
+          PlayingCard(cardSuit: CardSuit.diamonds, cardType: CardType.five));
+      player.recordGame(PositionOnScreen.bottom);
+      expect(player.personalInfo.avgScore, 40.0);
+    });
   });
 }
