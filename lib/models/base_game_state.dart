da3f8209d83bdd2f5b547e162c83e4c2d4b68124
alnah005 <alnah005@umn.edu>
2020-08-27 18:17:00 -0500

    fixed documentation for dartdoc

--- lib/models/base_game_state.dart
+++ lib/models/base_game_state.dart
@@ -8,10 +8,9 @@ abstract class BaseGameState<T> {
   List<T> _playerGameInfo;
   BaseRoundState<T> _round;
 
-  /*
-  This method is called to initialize GameState
-  Mainly called to initialize GameState (method constructor)
-   */
+  /// This method is called to initialize GameState
+  ///
+  ///Mainly called to initialize GameState (method constructor)
   void initializeGame(
       [int numOfPlayers,
       int numOfRounds,
@@ -19,62 +18,47 @@ abstract class BaseGameState<T> {
       List<T> playerInfo,
       BaseRoundState roundState]);
 
-  /*
-  This method is called to initialize round (cards, scores...etc)
-  Mainly called to initialize Round (method constructor)
-
-  Parameters:
-      playerList: contains the list of players in the game
-
-  Returns:
-      player in current turn
-   */
+  /// This method is called to initialize round (cards, scores...etc)
+  ///
+  /// Mainly called to initialize Round (method constructor)
+  ///
+  /// Parameters:
+  /// [playerList]: contains the list of players in the game
+  ///
+  /// Returns:
+  ///    player in current turn
   Player initializeRound(List<Player> playerList);
 
-  /*
-   Check if current player has won and update next round stats
-        for all players and update round
-
-   Returns:
-        True if current player has won
-        False if not
-   */
+  /// Check if current player has won and update next round state
+  /// for all players and update round
+  ///
+  /// Returns:
+  ///     True if current player has won
+  ///     False if not
   bool checkRoundWin();
 
-  /*
-   Check if a player has lost a game and record all players stats
-
-   Returns:
-        True if a player has lost
-        False if not
-   */
+  /// Check if a player has lost a game and record all players stats
+  ///
+  /// Returns:
+  ///     True if a player has lost
+  ///     False if not
   bool checkGameWin();
 
-  /*
-  * If current player hasn't won, go to next player to continue game
-  *
-  * Returns:
-  *     Player next in line to current player
-  */
+  /// If current player hasn't won, go to next player to continue game
+  ///
+  /// Returns:
+  ///     Player next in line to current player
   Player nextPlayer();
 
-  /*
-  call this method after a round has finished to update the round variable
-   */
+  /// call this method after a round has finished to update the round variable
   void trackRoundStats();
 
-  /*
-  call this method after the game has finished to record player stats
-   */
+  /// call this method after the game has finished to record player stats
   void trackGameStats();
 
-  /*
-   call this to kick a player out of the game
-   */
+  /// call this to kick a player out of the game
   void removePlayer(Player player);
 
-  /*
-   call this to add a player into the game
-   */
+  /// call this to add a player into the game
   void addPlayer(Player player, T playerInfo);
 }
