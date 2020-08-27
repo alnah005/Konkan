f10c6940dc6bb5b4a145aec158e019ea4f76ad0c
alnah005 <alnah005@umn.edu>
2020-08-27 18:18:46 -0500

    fixed documentation for dartdoc

--- lib/models/base_round_state.dart
+++ lib/models/base_round_state.dart
@@ -4,37 +4,29 @@ abstract class BaseRoundState<T> {
   Player _currentPlayer;
   T _playerGameInfo;
 
-  /*
-  initialize variables in round and start round
-   */
+  // initialize variables in round and start round
   void initializeRound();
 
-  /*
-  call this method after a round has finished to start a new round
-
-  Parameters:
-      previousRound: previous round state
-
-  Returns:
-      RoundState which is the Updated round state for next round
-   */
+  /// call this method after a round has finished to start a new round
+  ///
+  /// Parameters:
+  ///    [previousRound]: previous round state
+  ///
+  /// Returns:
+  ///    RoundState which is the Updated round state for next round
   BaseRoundState nextRound(BaseRoundState previousRound);
 
-  /*
-  call this method after a turn has finished to switch player and his info
-
-  Parameters:
-        nextPlayer: player next in line to replace current player
-        playerInfo: information about the player in the game
-   */
+  /// call this method after a turn has finished to switch player and his info
+  ///
+  /// Parameters:
+  ///      [nextPlayer]: player next in line to replace current player
+  ///      [playerInfo]: information about the player in the game
   void nextTurnVariables(Player nextPlayer, T playerInfo) {
     this._trackTurnStats();
     _currentPlayer = nextPlayer;
     _playerGameInfo = playerInfo;
   }
 
-  /*
-  call this method after a turn has finished to track player info
-   */
+  /// call this method after a turn has finished to track player info
   void _trackTurnStats();
 }
